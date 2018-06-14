5ee359d3567cc31bf6d510100f4971219100ba15
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-06-14 11:18:35 +0200

    Plays

--- state.pl
+++ state.pl
@@ -3,6 +3,8 @@
 apply_move(Before, move(move, From, To), After) :-
   board(Before, BeforeBoard),
 
+  board:piece_at(BeforeBoard, From, MovedPiece),
+
   % Board
   board:move_piece(BeforeBoard, From, To, AfterBoard),
   update_board(Before, AfterBoard, BoardState),
@@ -11,10 +13,10 @@ apply_move(Before, move(move, From, To), After) :-
   update_turn(BoardState, TurnState),
 
   % Half count
-  increase_halfcount(TurnState, HCState),
+  inc_halfcount(TurnState, MovedPiece, HCState),
 
   % Full count
-  increase_fullcount(HCState, After).
+  inc_fullcount(HCState, After).
 
 board([Board | _], Board).
 
@@ -25,13 +27,14 @@ en_passant([_, _, _, EP | _], EP).
 full_count([_, _, _, _, _, FC], FC).
 
 % white is the next player -> black played in this move
-increase_fullcount([B, white, C, EP, HC, FC], [B, white, C, EP, HC, FC1]) :-
+inc_fullcount([B, white, C, EP, HC, FC], [B, white, C, EP, HC, FC1]) :-
   succ(FC, FC1).
-increase_fullcount([B, black, C, EP, HC, FC], [B, black, C, EP, HC, FC]).
+inc_fullcount([B, black, C, EP, HC, FC], [B, black, C, EP, HC, FC]).
 
 half_count([_, _, _, _, HC | _], HC).
 
-increase_halfcount([B, T, C, EP, HC, FC], [B, T, C, EP, HC1, FC]) :-
+inc_halfcount([B, T, C, EP, _, FC], piece(pawn, _), [B, T, C, EP, 0, FC]) :- !.
+inc_halfcount([B, T, C, EP, HC, FC], _, [B, T, C, EP, HC1, FC]) :-
   succ(HC, HC1).
 
 turn([_, Turn | _], Turn).
