93fc6349e9611395c679d61d8dbc4da27deca2a5
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-06-20 11:55:50 +0200

    Scorefn

--- minimax.pl
+++ minimax.pl
@@ -3,7 +3,9 @@
 :- use_module(movement).
 :- use_module(state).
 
-best([State], Player, Depth, State, Score) :- minimax(State, Player, Depth, _, Score), !.
+best([State], Player, Depth, State, Score) :-
+  Deeper is Depth - 1,
+  minimax(State, Player, Deeper, _, Score), !.
 
 best([State1 | States], Player, Depth, BestState, BestScore) :-
   Deeper is Depth - 1,
@@ -27,8 +29,8 @@ minimax(Initial, Depth, Next) :-
   state:turn(Initial, Player),
   minimax(Initial, Player, Depth, Next, _).
 
-minimax(Initial, _, 0, _, Score) :-
-  score(Initial, Score), !.
+minimax(State, Player, 0, _, Score) :-
+  score(State, Player, Score), !.
 
 minimax(Current, Player, Depth, BestNextState, Score) :-
   movement:all_moves(Current, NextMoves),
@@ -36,7 +38,35 @@ minimax(Current, Player, Depth, BestNextState, Score) :-
   best(NextStates, Player, Depth, BestNextState, Score), !.
 
 % geen moves meer
-minimax(Initial, _, _, _, Score) :-
-  score(Initial, Score).
+minimax(State, Player, _, _, Score) :-
+  score(State, Player, Score).
 
-score(_, 0).
+piece_score(bishop, 3).
+piece_score(king, 1000).
+piece_score(knight, 3).
+piece_score(pawn, 1).
+piece_score(queen, 9).
+piece_score(rook, 5).
+
+score(State, Player, Score) :-
+  state:enemy(Player, Enemy),
+  score_sub(State, Player, MyScore),
+  score_sub(State, Enemy, EnemyScore),
+  Score is MyScore - EnemyScore, write(Score).
+
+score_sub(State, Turn, Score) :-
+  findall(Type, state:piece_at(State, _, piece(Type, Turn)), Pieces),
+  maplist(piece_score, Pieces, PieceScores),
+  sum_list(PieceScores, Score).
+
+% score_sub(State, Player, Score) :-
+%   state:
+
+% piece_score()
+%
+% % score_sub(State, Player, Score) :-
+% %   score_sub(State, BlackScore).
+% %
+% % score_sub(State, Player, Score) :-
+% %
+% %   score_black(State, BlackScore).
