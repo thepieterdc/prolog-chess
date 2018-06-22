0617179a29845ff291f2bc09c88d6e4fce0432b1
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-06-22 11:47:03 +0200

    More docs

--- src/fen.pl
+++ src/fen.pl
@@ -13,26 +13,31 @@ parse(FenString, State) :-
   % Apply the DCG to the string to obtain the state, or reverse.
   phrase(state(State), FenString).
 
+% DCG parser for a state, based on the FEN specifications.
 state(state(Board, Turn, Castling, EnPassant, HalfCount, FullCount)) -->
   board(Board), " ", turn(Turn), " ", castlings(Castling),
   " ", en_passant(EnPassant), " ", half_count(HalfCount),
   " ", full_count(FullCount).
 
+% DCG parser for a board.
 board(board(R1, R2, R3, R4, R5, R6, R7, R8)) -->
   row(R8),"/",row(R7),"/",row(R6),"/",row(R5),
   "/",row(R4),"/",row(R3),"/",row(R2),"/",row(R1).
 
+% DCG parser for castling.
 castlings([]) --> "-", !.
 castlings(Cs) --> castling_possibilities(Cs).
 % castlings[] kan niet opnieuw gebruikt worden want dan zou K-kq ook geldig zijn
 castling_possibilities([]) --> [].
 castling_possibilities([C | Cs]) --> castling(C), castling_possibilities(Cs).
 
+% DCG parser for the castling possibilities.
 castling(castling(kingside, black)) --> "k".
 castling(castling(kingside, white)) --> "K".
 castling(castling(queenside, black)) --> "q".
 castling(castling(queenside, white)) --> "Q".
 
+% DCG parser for the en passant squares.
 en_passant(none) --> "-".
 en_passant(3/1) --> "a3".
 en_passant(3/2) --> "b3".
@@ -51,10 +56,13 @@ en_passant(6/6) --> "f6".
 en_passant(6/7) --> "g6".
 en_passant(6/8) --> "h6".
 
+% DCG parser for the full-move counter.
 full_count(N) --> integer(N), {N > 0}.
 
+% DCG parser for the half-move counter.
 half_count(N) --> integer(N), {N >= 0}.
 
+% DCG parser for pieces.
 piece(piece(bishop, black)) --> "b".
 piece(piece(bishop, white)) --> "B".
 piece(piece(king, black)) --> "k".
@@ -68,10 +76,13 @@ piece(piece(queen, white)) --> "Q".
 piece(piece(rook, black)) --> "r".
 piece(piece(rook, white)) --> "R".
 
+% DCG parser for pieces on a row.
 piece(Piece, Left, Left1) --> piece(Piece), {Left1 is Left-1, Left1 >= 0}.
 
+% DCG parser for pieces on a row.
 pieces([], 0) --> [].
 
+% DCG parser for empty squares on a row.
 pieces([none, none, none, none, none, none, none, none], 8) --> "8".
 pieces([none, none, none, none, none, none, none | X], I) --> "7", !,
   {I1 is I-7, I1 >= 0},
@@ -95,16 +106,20 @@ pieces([none | X], I) --> "1", !,
   {I1 is I-1, I1 >= 0},
   pieces(X, I1).
 
+% DCG parser for pieces on a row.
 pieces([H|R], Left) --> piece(H, Left, Left1), pieces(R, Left1).
 
+% DCG parser (FEN to state) for a row.
 row(row(A, B, C, D, E, F, G, H)) --> {var(A), !},
   pieces(Row, 8),
   {
     flatten(Row, [A, B, C, D, E, F, G, H])
   }.
 
+% DCG parser (state to FEN) for a row.
 row(row(A, B, C, D, E, F, G, H)) --> {nonvar(A), !},
   pieces([A, B, C, D, E, F, G, H], 8).
 
+% DCG parser for turns.
 turn(black) --> "b".
 turn(white) --> "w".
--- src/movement/bishop.pl
+++ src/movement/bishop.pl
@@ -4,19 +4,44 @@
 :- use_module('../state').
 :- use_module(positions).
 
-% Bishop capture.
+%% move(+Board: board, +Turn: turn, +move:move: +move:move) is semidet
+%
+%  Formulates a capture move.
+%
+%  @param Board the current board
+%  @param Turn the owner of the bishop
+%  @param move the unprocessed move
+%  @param move(capture) the capturing move
 move(Board, Turn, move(From, _, To), move(capture, From, To)) :-
+  % Verify the destination square contains an enemy piece.
   board:enemy(Board, To, Turn), !.
 
-% Bishop walk.
+%% move(+Board: board, +Turn: turn, +move:move: +move:move) is semidet
+%
+%  Formulates a regular walking move.
+%
+%  @param Board the current board
+%  @param Turn the owner of the bishop
+%  @param move the unprocessed move
+%  @param move(move) the walking move 
 move(Board, _, move(From, _, To), move(move, From, To)) :-
+  % Verify the destination square does not contain any piece.
   board:free(Board, To).
 
+%% moves(+State: state, +Square: square, +Turn: turn, -Moves: list) is semidet
+%
+%  Finds all valid moves for a bishop on the given Square.
+%
+%  @param State the current game state
+%  @param Square the square that contains a bishop
+%  @param Turn the owner of the bishop
+%  @param Moves the resulting available moves
 moves(State, Square, Turn, Moves) :-
+  % Extract the board from the state.
   state:board(State, Board),
-
+  % Get all bishop moves from the current square.
   positions:bishop_moves(Square, BishopMoves),
-
+  % Validate every found bishop move.
   include(movement:path_clear(Board), BishopMoves, FilteredMoves),
-
+  % Convert the moves to either a move or a capture.
   convlist(move(Board, Turn), FilteredMoves, Moves).
